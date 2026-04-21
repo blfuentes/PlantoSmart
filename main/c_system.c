a212a2a909176717eac129d75f9980262a5619eb
blfuentes <epiphonetodie@gmail.com>
2026-04-21 09:59:13 +0200

    dev: working on display

--- main/c_system.c
+++ main/c_system.c
@@ -4,11 +4,8 @@
 #include <driver/i2c_master.h>
 #include <stdlib.h>
 
-#define DISPLAY_GPIO_SDA_PIN GPIO_NUM_6
-#define DISPLAY_GPIO_SCL_PIN GPIO_NUM_7
-
-static SystemDevs system_devs = {.display_sda_pin = DISPLAY_GPIO_SDA_PIN,
-                                 .display_scl_pin = DISPLAY_GPIO_SCL_PIN};
+static SystemDevs system_devs = {.display_sda_pin = CONFIG_SDA_GPIO,
+                                 .display_scl_pin = CONFIG_SCL_GPIO};
 SystemDevs* system_init(void) {
     // Initialize display
     display_init(&system_devs.display);
--- main/display.c
+++ main/display.c
@@ -11,15 +11,17 @@ void display_init(Display* display) {
     ESP_LOGI(SCREEN_TAG, "CONFIG_SDA_GPIO=%d", CONFIG_SDA_GPIO);
     ESP_LOGI(SCREEN_TAG, "CONFIG_SCL_GPIO=%d", CONFIG_SCL_GPIO);
     ESP_LOGI(SCREEN_TAG, "CONFIG_RESET_GPIO=%d", CONFIG_RESET_GPIO);
-    ESP_LOGI(SCREEN_TAG, "Panel is 128x32");
+    ESP_LOGI(SCREEN_TAG, "Panel is 128x64");
 
     i2c_master_init(&display->dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
 
-    ssd1306_init(&display->dev, 128, 32);
+    ssd1306_init(&display->dev, 128, 64);
     ssd1306_clear_screen(&display->dev, false);
     ssd1306_contrast(&display->dev, 0xff);
-    char starting_text[] = "STARTING...";
-    ssd1306_display_text(&display->dev, 0, starting_text, 9, false);
+    // char starting_text[] = "LOS VEGGIES...";
+    char starting_text[] = "I love you bubu";
+
+    ssd1306_display_text(&display->dev, 0, starting_text, 18, false);
 
     ESP_LOGI(SCREEN_TAG, "SSD1306 initialized");
 }
